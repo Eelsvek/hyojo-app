392eb42204e1d1d9d5f3a568041e660151bdeffe
eelsvek <kev.sunlee@gmail.com>
2020-09-02 22:13:41 -0700

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div></div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
