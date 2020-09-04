ea70a361746c09b0b30746260aa94b7caf8c65e7
eelsvek <kev.sunlee@gmail.com>
2020-09-04 15:33:17 -0700

    setup login form

--- src/components/Forms/LoginForm.vue
+++ src/components/Forms/LoginForm.vue
@@ -1,52 +1,53 @@
 <template>
   <div>
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form @submit="onSubmit" class="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
-        <label class="block text-primary-500 text-sm font-bold mb-2" for="username">
-          Username
-        </label>
+        <label class="block text-primary-500 text-sm font-bold mb-2" for="email">Email</label>
         <input
+          v-model="email"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
+          id="email"
           type="text"
-          placeholder="Username"
+          placeholder="Email"
         />
       </div>
       <div class="mb-6">
-        <label class="block text-primary-500 text-sm font-bold mb-2" for="password">
-          Password
-        </label>
+        <label class="block text-primary-500 text-sm font-bold mb-2" for="password">Password</label>
         <input
+          v-model="password"
           class="shadow appearance-none border border-error rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
           type="password"
-          placeholder="******************"
+          placeholder="*****"
         />
         <p class="text-error text-xs italic">Please choose a password.</p>
       </div>
       <div class="flex items-center justify-between">
         <button
           class="bg-primary-500 hover:bg-primary-300 text-white text-sm font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-        >
-          Sign In
-        </button>
-        <a
-          class="inline-block align-baseline font-bold text-sm text-primary hover:text-primary-300"
-          href="#"
-        >
-          Forgot Password?
-        </a>
+          type="submit"
+        >Sign In</button>
       </div>
     </form>
-    <p class="text-center text-gray-500 text-xs">
-      &copy;2020 SCMP LLC.
-    </p>
+    <p class="text-center text-gray-500 text-xs">&copy;2020 SCMP LLC.</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      console.log('wooo', this.email, this.password);
+    },
+  },
+};
 </script>
 
 <style></style>
