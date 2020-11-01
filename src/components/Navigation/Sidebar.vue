a07735144417d8f506aa416c1949cc1a138cc227
eelsvek <kev.sunlee@gmail.com>
2020-11-01 14:43:37 -0800

    layout

---
+++ src/components/Navigation/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="sidebar" class="flex flex-col w-56 h-screen bg-secondary-300">
+    <a
+      v-for="item in sidebarItems"
+      :key="item.url"
+      class="px-2 py-2 color-white cursor-pointer"
+      >{{ item.title }}</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sidebarItems: [
+        {
+          title: 'Home',
+          url: '/',
+        },
+        {
+          title: 'Following',
+          url: '/directory/following',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <MainMenu />
-    <slot> </slot>
+  <div id="main-layout" class="flex">
+    <div class="flex flex-col">
+      <Sidebar />
+    </div>
+    <div class="flex flex-1 flex-col">
+      <MainMenu />
+      <div class="p-8">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import MainMenu from '@/components/Navigation/MainMenu';
+import Sidebar from '@/components/Navigation/Sidebar';
 
 export default {
   components: {
     MainMenu,
+    Sidebar,
   },
 };
 </script>
