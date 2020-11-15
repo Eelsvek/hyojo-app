73e8d4350558bd7dd29437a7ac318f2e4c8203db
eelsvek <kev.sunlee@gmail.com>
2020-11-14 16:34:33 -0800

    get vuex working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
-import Vuex from 'vuex';
 
 import App from './App.vue';
 import './assets/main.scss';
@@ -11,7 +10,6 @@ import store from './store';
 Vue.config.productionTip = false;
 
 Vue.use(Vuelidate);
-Vue.use(Vuex);
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vuex from 'vuex';
+import Vue from 'vue';
 
 import Modals from './modules/modals';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
